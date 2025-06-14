68bc83f92ff58f0c243f8aad173cf62a64fc9117
Han <147114637+hanhercales@users.noreply.github.com>
2025-06-15 02:57:09 +0700

    Edit some Canvas settings

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -8,9 +8,12 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] private string collectedForms = "CollectedForms";
     [SerializeField] private Button continueButton;
+    [SerializeField] private AudioSource audioSource;
 
     private void Start()
     {
+        audioSource = GameObject.Find("GameManager").GetComponent<AudioSource>();
+        
         if(!PlayerPrefs.HasKey(collectedForms))
             continueButton.interactable = false;
     }
@@ -22,5 +25,10 @@ public class MenuManager : MonoBehaviour
             PlayerPrefs.DeleteKey(collectedForms);
             PlayerPrefs.Save();
         }
+    }
+
+    public void SetAudioVolumn(float volumn)
+    {
+        audioSource.volume = volumn;
     }
 }
